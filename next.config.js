4640c9f372b99dcbf6a172640f7e727a8627a031
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-01-04 18:19:02 +0330

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'your-image-hostname.com',
+        port: '',
+        pathname: '/public/images/**',
+      },
+    ],
+  },
+};
